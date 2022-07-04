e17a441a9729a9250169a22fb6406700dd012c18
MATHEUS BATISTA <78868039+matheusbat28@users.noreply.github.com>
2022-07-03 22:23:50 -0300

    caixa alta

--- src/function/funticions.py
+++ src/function/funticions.py
@@ -1,25 +1,28 @@
+
 def value_by_dict(list, list_Name, name):
     cont = 0
+    try:
+        if name == 'Filmes':
+            for x in list_Name: 
+                dict_filmes = {}
+                dict_filmes[name] = x
+                list.append(dict_filmes)
 
-    if name == 'Filmes':
-        for x in list_Name: 
-            dict_filmes = {}
-            dict_filmes[name] = x
-            list.append(dict_filmes)
-
-    elif name == 'Status':
-        for x in list_Name: 
-            x_str = str(x)
-            if x_str == "nan":
-                list[cont][name] = ' '  
-            else:        
-                list[cont][name] = x_str
-            cont += 1
-    else:
-        for x in list_Name: 
-            x_str = str(x)
-            if x_str == "nan":
-                list[cont][name] = '0'  
-            else:        
-                list[cont][name] = x_str
-            cont += 1
+        elif name == 'Status':
+            for x in list_Name: 
+                x_str = str(x)
+                if x_str == "nan":
+                    list[cont][name] = ' '  
+                else:        
+                    list[cont][name] = x_str.capitalize()
+                cont += 1
+        else:
+            for x in list_Name: 
+                x_str = str(x)
+                if x_str == "nan":
+                    list[cont][name] = '0'  
+                else:        
+                    list[cont][name] = x_str.capitalize()
+                cont += 1
+    except:
+        print("erro ao executar")
