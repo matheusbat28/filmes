b9cd7ac35aa16bac7e8f3a87023416a6fc84ddee
MATHEUS BATISTA <78868039+matheusbat28@users.noreply.github.com>
2022-07-03 22:48:15 -0300

    token api google

---
+++ src/apis/google.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+SAMPLE_SPREADSHEET_ID = '1RRTqLZWXDEU1gJttXyYwLo0tjFdnBF6pqV7Z_MGpEoU'
+SAMPLE_RANGE_NAME = 'Página1!A1:I151'
+
+
+def main():
+
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secrets.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+'''
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        print('Name, Major:')
+        for row in values:
+            print('%s, %s' % (row[0], row[4]))
+    except HttpError as err:
+        print(err)
+'''
+
+if __name__ == '__main__':
+    main()
