1286cf11d195b1b4b27f873e5e13cb31fbbc82aa
MATHEUS BATISTA <78868039+matheusbat28@users.noreply.github.com>
2022-07-04 21:36:28 -0300

    executal

--- src/treatment.py
+++ src/treatment.py
@@ -3,8 +3,10 @@ import json
 import os
 from function.funticions import value_by_dict
 from apis.google import main
+from screens.index import save
 
 main()
+path_file_save = save()
 path_file = "src/files/notas_filmes.xlsx"
 list = []
 dict_json = {}
@@ -21,7 +23,9 @@ for x in list:
     dict_json[f'f{cont}'] = x
     cont += 1 
 
-with open('src/files/filmes.json', 'w', encoding="UTF-8") as file_json:
+with open(path_file_save, 'w', encoding="UTF-8") as file_json:
     file_json.write(json.dumps(dict_json))
 
 os.remove(path_file)
+print(f"local do arquivo salvo: {path_file_save}")
+print('------------------------FIM------------------------')
